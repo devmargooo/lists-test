f239a0a2640d29eb40aedb4e1db5d578af7b5fe2
Маргарита Лукина <mlukina@Mbp-mlukina.local>
2021-11-07 20:24:37 +0300

    lists test

--- src/list/List.ts
+++ src/list/List.ts
@@ -1,5 +1,22 @@
-export class List {
-    test() {
-        return 10;
+export class List<T> {
+    add(value:T) {
+
     }
+
+    find(index:number):T {
+
+    }
+
+    insert(index:number, value: T) {
+
+    }
+
+    findIndex(value: T):number {
+
+    }
+
+    remove(index: number) {
+
+    }
+    public length:number = 0;
 }
