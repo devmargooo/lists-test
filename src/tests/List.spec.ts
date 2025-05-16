1b49a03e79c0bb62cb2d9f0a020a8c0023c1d56a
devmargooo <devmargooo@gmail.com>
2025-05-16 11:17:27 +0300

    fix README

--- src/tests/List.spec.ts
+++ src/tests/List.spec.ts
@@ -1,76 +1,74 @@
-import {List} from "../list/List";
+import { List } from '../list/List';
 
 type Value = {
-    id: number;
-    name: string;
-}
+  id: number;
+  name: string;
+};
 
-const first = {id: 1, name: 'первый'};
-const second = {id: 2, name: 'второй'};
-const third = {id: 3, name: 'третий'};
-const new_third = {id: 4, name: 'другой трейти'}ж
+const first = { id: 1, name: 'первый' };
+const second = { id: 2, name: 'второй' };
+const third = { id: 3, name: 'третий' };
+const new_third = { id: 4, name: 'другой трейти' };
 
 interface INode<T> {
-    next: T;
-    prev: T;
+  next: T;
+  prev: T;
 }
-describe("List", () => {
-    const list = new List<Value>();
-    list.add({id: 1, name: 'первый'});
-    list.add({id: 2, name: 'второй'});
-    list.add({id: 3, name: 'третий'});
+describe('List', () => {
+  const list = new List<Value>();
+  list.add({ id: 1, name: 'первый' });
+  list.add({ id: 2, name: 'второй' });
+  list.add({ id: 3, name: 'третий' });
 
-    it("Правильно считает длину", () => {
-        expect(list.length).toBe(3);
-    });
+  it('Правильно считает длину', () => {
+    expect(list.length).toBe(3);
+  });
 
-    it("Верны указатели next", () => {
-        expect((first as unknown as INode<Value>).next).toBe(second);
-        expect((second as unknown as INode<Value>).next).toBe(third);
-    });
+  it('Верны указатели next', () => {
+    expect((first as unknown as INode<Value>).next).toBe(second);
+    expect((second as unknown as INode<Value>).next).toBe(third);
+  });
 
-    it("Верны указатели prev", () => {
-        expect((second as unknown as INode<Value>).prev).toBe(first);
-        expect((third as unknown as INode<Value>).next).toBe(second);
-    });
+  it('Верны указатели prev', () => {
+    expect((second as unknown as INode<Value>).prev).toBe(first);
+    expect((third as unknown as INode<Value>).next).toBe(second);
+  });
 
-    it("Верен указатель head", () => {
-        expect(list.head).toBe(first);
-    });
+  it('Верен указатель head', () => {
+    expect(list.head).toBe(first);
+  });
 
-    it("Верен указатель tail", () => {
-        expect(list.tail).toBe(third);
-    });
+  it('Верен указатель tail', () => {
+    expect(list.tail).toBe(third);
+  });
 
-    it("Находит элемент по индексу", () => {
-        expect(list.find(0)).toBe(first);
-        expect(list.find(1)).toBe(second);
-        expect(list.find(2)).toBe(third);
-    });
+  it('Находит элемент по индексу', () => {
+    expect(list.find(0)).toBe(first);
+    expect(list.find(1)).toBe(second);
+    expect(list.find(2)).toBe(third);
+  });
 
-    it("Правильно вставляет элемент по индексу", () => {
-        list.insert(2, new_third);
-        expect(list.length).toBe(4);
-        expect(list.find(0)).toBe(first);
-        expect(list.find(1)).toBe(second);
-        expect(list.find(2)).toBe(new_third);
-        expect(list.find(3)).toBe(third);
-    });
+  it('Правильно вставляет элемент по индексу', () => {
+    list.insert(2, new_third);
+    expect(list.length).toBe(4);
+    expect(list.find(0)).toBe(first);
+    expect(list.find(1)).toBe(second);
+    expect(list.find(2)).toBe(new_third);
+    expect(list.find(3)).toBe(third);
+  });
 
-    it("Находит индекс элемента", () => {
-        expect(list.findIndex(first)).toBe(0);
-        expect(list.findIndex(second)).toBe(1);
-        expect(list.findIndex(new_third)).toBe(2);
-        expect(list.findIndex(third)).toBe(3);
-    });
+  it('Находит индекс элемента', () => {
+    expect(list.findIndex(first)).toBe(0);
+    expect(list.findIndex(second)).toBe(1);
+    expect(list.findIndex(new_third)).toBe(2);
+    expect(list.findIndex(third)).toBe(3);
+  });
 
-    it("Удаляет элемент по индексу", () => {
-        list.remove(2);
-        expect(list.length).toBe(3);
-        expect(list.find(0)).toBe(first);
-        expect(list.find(1)).toBe(second);
-        expect(list.find(2)).toBe(third);
-    });
-
-
+  it('Удаляет элемент по индексу', () => {
+    list.remove(2);
+    expect(list.length).toBe(3);
+    expect(list.find(0)).toBe(first);
+    expect(list.find(1)).toBe(second);
+    expect(list.find(2)).toBe(third);
+  });
 });
